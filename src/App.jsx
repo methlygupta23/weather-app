34dc996eaff625ece9224dcf6e4bb1379a40e74e
methlygupta23 <methlygupta7@gmail.com>
2025-08-01 02:29:05 +0530

    weather app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import './index.css';
+import Weather from './component/weather';
+
+
+const App = () => {
+  
+  return (
+    <div className="app">
+      <Weather/>
+    </div>
+  );
+};
+
+export default App;
